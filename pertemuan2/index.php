f9e1948b05ac1e79419340c9aeade17a0db1efc3
berlianaputri8 <berlianaap08@gmail.com>
2021-02-07 18:46:23 +0700

    pw2021

---
+++ pertemuan2/index.php
@@ -0,0 +1,87 @@
+<?php
+
+/*
+    Berliana Putri
+    203040056
+    https://github.com/berlianaputri8/pw2021_203040056.git
+    Pertemuan 2 (7 Januari 2021)
+    Materi minggu ini mempelajari mengenai sintaks PHP
+
+*/
+
+// standar output
+// echo, print
+    // echo "Berliana Putri";
+    // echo true;
+    // echo false;
+    // print "Berliana Putri";
+// print_r
+    // print_r ("Berliana Putri");
+// var_dump
+    // var_dump ("Berliana Putri");
+
+
+// Penulisan sintaks PHP
+// 1. PHP di dalam HTML
+// 2. HTML di dalam PHP
+
+// Variabel dan Tipe Data 
+// Variabel
+// tidak boleh diawali dengan angka, tapi boleh mengandung angka
+    // $nama = "Berliana Putri";
+    // echo "Hallo, nama saya $nama";
+
+// Operator
+// Aritmatika
+// + - * / %
+    // echo 1 + 1;
+    // $x = 10;
+    // $y = 20;
+    // echo $x * $y;
+
+// Penggabung String / Concatenation / concat 
+// .
+    // $nama_depan = "Berliana";
+    // $nama_belakang = "Putri";
+    // echo $nama_depan ." " . $nama_belakang;
+
+// Assignment
+// =, +=, -=, *=, /=, %=, .=
+    // $x = 1;
+    // $x -= 5;
+    // echo $x;
+
+    // $nama = "Berliana";
+    // $nama .= " ";
+    // $nama .= "Putri";
+    // echo $nama;
+
+// Perbandingan
+// <, >, <=, >=, ==, !=
+    // var_dump(1 == "1");
+
+// Identitas
+// ===, !==
+    // var_dump(1 === "1")
+
+// Logika
+// &&, ||, !
+    // $x = 30;
+    // var_dump($x < 20 || $x % 2 == 0);
+
+?>
+ 
+<!-- <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Belajar PHP</title>
+</head>
+<body>
+    <h1>Hallo, Selamat Datang <?php echo $nama; ?></h1>
+    <p><?php echo "ini adalah paragraf"; ?></p>
+
+</body>
+</html> -->
+
